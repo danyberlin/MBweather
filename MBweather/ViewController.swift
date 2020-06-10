35ec1704ce8348f663ffaa1ce7be943e5552bd81
Dany Gold <danyberlin@hotmail.com>
2020-06-10 16:39:59 +0200

    Initial Commit

---
+++ MBweather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MBweather
+//
+//  Created by Daniel Goldgamer on 10.06.20.
+//  Copyright © 2020 Daniel Goldgamer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
